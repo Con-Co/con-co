ffaf5c2834e70eaacfa95b530c67a5f9ad5926b0
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-02-19 18:52:45 -0600

    Delete PartnerShip-main directory

--- PartnerShip-main/dashboard.js
+++
@@ -1,9 +0,0 @@
-var url="/PartnerShip/dashboard.html",xmlhttp;//Remember, same domain
-if("XMLHttpRequest" in window)xmlhttp=new XMLHttpRequest();
-if("ActiveXObject" in window)xmlhttp=new ActiveXObject("Msxml2.XMLHTTP");
-xmlhttp.open('GET',url,true);
-xmlhttp.onreadystatechange=function()
-{
-    if(xmlhttp.readyState==4)document.write(xmlhttp.responseText);
-};
-xmlhttp.send(null);
